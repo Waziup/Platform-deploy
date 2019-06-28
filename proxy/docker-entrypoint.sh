178e819dfcb68cda78a8dbca0a41805cdb100b4e
Corentin Dupont <corentin.dupont@gmail.com>
2019-06-28 18:07:06 +0200

    clean

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh 
-envsubst '$$WAZIUP_BASE_URL' < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf
-cat /etc/nginx/conf.d/default.conf
+envsubst '$$WAZIUP_BASE_URL' < /etc/nginx/conf.d/waziup.conf.template > /etc/nginx/conf.d/waziup.conf
+cat /etc/nginx/conf.d/waziup.conf
 nginx -g 'daemon off;'
